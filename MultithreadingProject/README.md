69aef2e592ec6081f4b3cb853832582fb85d0d6d
Layeeq Ahmed <layeeq.ahmed@accoliteindia.com>
2019-01-21 01:24:03 +0530

    Multithreading Assignment Added

---
+++ MultithreadingProject/README.md
@@ -0,0 +1,8 @@
+Two threads are run -> one for passengers, one for taxis.
+New passenger is generated with a random destination every 3 seconds.
+New taxi arrives every 60 seconds.
+(change these values in respective "Runnable" files in Thread.sleep(int) method)
+Taxi Stand has capacity of 5 taxis.
+If a new passenger cannot be assigned a taxi from the taxi stand, he/she is added to passengerQueue.
+
+TaxiStand class has main method to run the simulation.
---
+++ MultithreadingProject/src/com/assignment/taxi/TaxiQueueRunnable.java
@@ -0,0 +1,31 @@
+package com.assignment.taxi;
+
+import java.util.LinkedList;
+
+public class TaxiQueueRunnable implements Runnable {
+
+    private LinkedList<Taxi> taxiQueue;
+    private TaxiStand taxiStand;
+
+    public TaxiQueueRunnable(LinkedList<Taxi> q, TaxiStand l){
+        taxiQueue = q;
+        taxiStand = l;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Starting taxiQueue Thread...");
+        while(true) {
+            try {
+                Thread.sleep(60000);
+            } catch (InterruptedException ie) {
+            	System.out.println("Interruppted Exception Occurred");
+            }
+            
+            Taxi temp = new Taxi(null);
+            if(taxiStand.addTaxi(temp))
+            	System.out.println("Taxi added to Stand");
+            else System.out.println("Taxi added to Queue");
+        }
+    }
+}
