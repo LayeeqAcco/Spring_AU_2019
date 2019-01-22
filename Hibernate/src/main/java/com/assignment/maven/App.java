9aa8ca86af28f94a953dca7103e2f8daebf3a237
Layeeq Ahmed <layeeq.ahmed@accoliteindia.com>
2019-01-23 04:04:02 +0530

    Hibernate Assignment Added

---
+++ Hibernate/src/main/java/com/assignment/maven/App.java
@@ -0,0 +1,87 @@
+package com.assignment.maven;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.assignment.maven.POJO.Student;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	List<Student>studentList= new ArrayList<Student>();
+    	
+    	SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session=factory.openSession();
+        session.beginTransaction();
+        
+        //uncomment below code and modify to add new student
+        
+        /*
+        Student stud = new Student();
+		stud.setStudentId(4);
+		stud.setStudentName("Kirti");
+		stud.setStudentAge(21);
+		session.save(stud);
+		*/
+        /*
+        Student stud = new Student();
+		stud.setStudentId(5);
+		stud.setStudentName("Krutika");
+		stud.setStudentAge(22);
+		session.save(stud);
+        */
+        /*
+        Student stud = new Student();
+		stud.setStudentId(3);
+		stud.setStudentName("Ayushi");
+		stud.setStudentAge(22);
+		session.save(stud);
+		*/
+        //refer StudentTable screenshot
+        
+        /*
+        Course c = new Course();
+		c.setCourseName("Maven");
+		c.setStudentId(4);
+		session.save(c);
+        */
+        /*
+        Course c = new Course();
+		c.setCourseName("Maven");
+		c.setStudentId(5);
+		session.save(c);
+		*/
+        /*
+		Course c = new Course();
+		c.setCourseName("Spring");
+		c.setStudentId(4);
+		session.save(c);
+        */
+        /*
+        Course c = new Course();
+		c.setCourseName("Spring");
+		c.setStudentId(3);
+		session.save(c);
+		*/
+        
+        @SuppressWarnings("deprecation")
+		Criteria crit = session.createCriteria(Student.class);
+        studentList= crit.list();
+        System.out.println("NAME|AGE|ID|COURSE_NAMES");
+        for(Student student: studentList) {
+   		 System.out.println( student.getStudentName()+"|" +student.getStudentAge()+"|"
+   		 +student.getStudentId()+"|"
+   		 +(!student.getCourses().isEmpty()?student.getCourses().get(0).getCourseName()
+   		 :"") ); 
+   		 }
+        
+		session.getTransaction().commit();
+		
+    }
+}
