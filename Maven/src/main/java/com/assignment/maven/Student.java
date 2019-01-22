5a976ac7a3d1a4e675d6daece7f23a5ec23e8480
Layeeq Ahmed <layeeq.ahmed@accoliteindia.com>
2019-01-23 03:13:31 +0530

    Maven Assignment Added

---
+++ Maven/src/main/java/com/assignment/maven/Student.java
@@ -0,0 +1,62 @@
+package com.assignment.maven;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student")
+public class Student {
+
+	@Id
+	@Column(name="studentId")
+	public int studentId;
+
+	@Column(name="studentName")
+	public String studentName;
+
+	@Column(name="studentAge")
+	public int studentAge;
+
+	
+	 @OneToMany(mappedBy="studentId", fetch=FetchType.EAGER)
+	 public List<Course> courses;
+	 
+	
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(Integer studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public Integer getStudentAge() {
+		return studentAge;
+	}
+
+	public void setStudentAge(Integer studentAge) {
+		this.studentAge = studentAge;
+	}
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+}
